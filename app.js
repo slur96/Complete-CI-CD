f0e923a1201846a21c17245b85a198bfc8ab17dd
sam-techy <samuel@sam-techy.info>
2024-11-12 22:17:15 +0000

    Mod JF

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  res.send('Hello, World!.\n');
+  res.send('Hello, Sam!.\n');
 });
 
 app.listen(port, () => {
